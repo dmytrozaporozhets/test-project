f6b4bafc5c9bb93d47b1c0474d4e3ba203c00a28
dimazaporozhets <dimkazaporozhets@gmail.com>
2023-11-17 14:20:11 +0200

    Added Eslint configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
