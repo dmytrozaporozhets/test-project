40a973f7953e457bc8e6221eb88a9a1da954f6b7
dimazaporozhets <dimkazaporozhets@gmail.com>
2023-11-23 11:42:12 +0200

    Added safe area (react-native-safe-area-context).

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,18 +3,21 @@ import { Button, StyleSheet, Text, View } from "react-native";
 import { Routes } from "../navigation/Routes";
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../navigation/ParamsList";
+import { ScreenView } from "../components";
 
 const Home = memo(() => {
   const { navigate, goBack } = useNavigation<RootStackParamList>();
   return (
-    <View style={styles.container}>
-      <Text>Home screen</Text>
-      <Button
-        title="Go to settings screen"
-        onPress={() => navigate(Routes.SETTINGS)}
-      />
-      <Button title="Go back to sign in screen" onPress={() => goBack()} />
-    </View>
+    <ScreenView>
+      <View style={styles.container}>
+        <Text>Home screen</Text>
+        <Button
+          title="Go to settings screen"
+          onPress={() => navigate(Routes.SETTINGS)}
+        />
+        <Button title="Go back to sign in screen" onPress={() => goBack()} />
+      </View>
+    </ScreenView>
   );
 });
 
